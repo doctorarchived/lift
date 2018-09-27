ac5c51d9e7f6d140cc035d4ae282c40e7ea36576
Michael <brady.m@outlook.com>
2018-09-26 23:54:07 -0400

    hello

--- lift/models/routine/routine.py
+++ lift/models/routine/routine.py
@@ -1,16 +1,77 @@
-from datetime import date
-from typing import Dict
+from datetime import date, datetime
+from typing import Dict, List
+
+
+class ExerciseTemplate:
+    name: str
+    id: int
+
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+    def __str__(self):
+        return f'[{self.id} | {self.name}]'
+
+
+class Set:
+    percent: float
+    weight: float
+    reps: int
+
+    def __init__(self, percent, reps):
+        self.percent = percent
+        self.reps = reps
+
+    def __repr__(self):
+        return f'{self.weight} x {self.reps}'
+
+
+class Exercise:
+    exercise_template: ExerciseTemplate
+    sets: List[Set]
+
+    def __init__(self, exercise_template=None, sets=None):
+        self.exercise_template = exercise_template
+        self.sets = sets
+
+    def __str__(self):
+        return f'{self.exercise_template} | {str(self.sets)}'
 
 
 class Workout:
-    pass
+    exercises: List[Exercise]
+
+    def __init__(self, exercises):
+        self.exercises = exercises
+
+    def show_me(self):
+        for exercise in self.exercises:
+            print(exercise)
 
 
 class Routine:
     routine: Dict[date, Workout] = {}
-
-
+    exercise_bases: Dict[int, float] = {}
 
     def add_workout(self, day, workout):
-        self.routine[day] = workout
+        for exercise in workout.exercises:
+            id = exercise.exercise_template.id
+            base = self.exercise_bases[id]
+            for s in exercise.sets:
+                s.weight = base * s.percent
 
+        self.routine[day] = workout
+        workout.show_me()
+
+
+if __name__ == '__main__':
+    template = ExerciseTemplate(1, 'Squat')
+
+    squat = Exercise(template, [Set(0.5, 10), Set(0.6, 8), Set(0.7, 6)])
+
+    workout = Workout([squat, ])
+
+    routine = Routine()
+    routine.exercise_bases = {1: 315}
+    routine.add_workout(datetime.today(), workout)
